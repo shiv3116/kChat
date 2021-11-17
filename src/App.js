775b6fb8fbfc07c2981f7355835bc664777dff7a
Shivam Sharma <60747392+shiv3116@users.noreply.github.com>
2021-11-17 11:39:01 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import AuthProvider from "./context/auth";
+import PrivateRoute from "./components/PrivateRoute";
+import Profile from "./pages/Profile";
+
+function App() {
+  return (
+    <AuthProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Switch>
+          <Route exact path="/Login" component={Login } />
+          <Route exact path="/Register" component={Register} />
+          <PrivateRoute exact path="/Profile" component={Profile} />
+          <PrivateRoute exact path="/" component={Home} />
+        </Switch>
+      </BrowserRouter>
+    </AuthProvider>
+  );
+}
+
+export default App;
